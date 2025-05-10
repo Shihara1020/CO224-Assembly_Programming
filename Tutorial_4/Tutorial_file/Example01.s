fab3312862216e7aa51f88746914bbda80554d04
Shihara Dewagedara <eranjan1020@gmail.com>
2025-05-10 14:00:08 +0530

    correct the ex01 file

--- Tutorial_4/Tutorial_file/Example01.s
+++ Tutorial_4/Tutorial_file/Example01.s
@@ -3,6 +3,12 @@
 
 main:
     sub sp,sp,#4
+    str lr,[sp,#0]
+
+    sub sp,sp,#4
+    ldr r0,=formats1
+    bl printf
+
     ldr r0, =formats
     mov r1,sp
     bl scanf
@@ -13,10 +19,14 @@ main:
     bl printf
 
     add sp,sp,#4
-    mov r7,#1
-    svc #0
+    
+exit:
+    ldr lr,[sp,#0]
+    add sp,sp,#4
+    mov pc,lr
 
 
 .data
+formats1: .asciz "Enter the number: "
 formats: .asciz "%d"
 formatp: .asciz "The number is %d\n"
